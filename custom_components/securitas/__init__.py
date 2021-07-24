116c97f174bbb693b1bbb724b45d7eb85ae4c5ff
vlumikero <vlumikero@hotmail.com>
2021-07-24 20:43:04 +0300

    Update __init__.py

--- custom_components/securitas/__init__.py
+++ custom_components/securitas/__init__.py
@@ -79,7 +79,8 @@ class SecuritasClientAPI(object):
         self._property_id = 0
         self._last_updated = 0
         self._panel_type = ''
-        self._target_state = STATE_ALARM_DISARMED
+        self._target_state = ''
+        #STATE_ALARM_DISARMED
         self._state = self.get_alarm_status()
 
     def _do_request(self, request_type, url, payload):
@@ -120,7 +121,9 @@ class SecuritasClientAPI(object):
         _LOGGER.debug("Get Securitas alarm state: %s", self._state)
         _LOGGER.debug("Target Securitas alarm state: %s", self._target_state)
         
-        if self._state == self._target_state:
+        if self._state == '':
+                return self._state
+        elif self._state == self._target_state:
         	return self._state
         else:
         	return STATE_ALARM_PENDING
